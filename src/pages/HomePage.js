0d32e03a7ecbe81d5772f32e7b8ead7a4faa1511
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-30 12:20:43 +0200

    Style the top most homepage div

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,13 @@
 const HomePage = () => (
   <div className="columns">
-    <div className="columns is-centered is-vcentered">
-      <div className="column is-4">
-        <h1>Find accomodation from anywhere</h1>
-        <p>
+    <div className="columns is-vcentered home-page-1">
+      <div className="column is-4 m-6">
+        <h1 className="is-size-2 has-text-white">Find accomodation from anywhere</h1>
+        <p className="is-size-5 has-text-white">
           The best offers for you at any point of your journey.
           Wherever you are, feel yourself like at your own home
         </p>
+        <button type="button" className="button is-rounded start-free-btn is-uppercase">Start free</button>
       </div>
     </div>
   </div>
