6397939d31244d7547d9e04b3c76246aeefb5d1c
descholar-ceo <emmanuellamugi@gmail.com>
2021-01-30 15:37:14 +0200

    Style the beofore footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,7 +15,10 @@ const HomePage = () => (
     <div className="columns is-vcentered is-centered home-page-2">
       <div className="column is-3">
         <img src={accommodationPic} alt="neza guest house" />
-        <h4>Neza Guest House</h4>
+        <div className="columns is-centered is-vcentered">
+          <h4 className="column is-half">Neza Guest House</h4>
+          <h4 className="column is-half is-pulled-right">$ 1221 / per month</h4>
+        </div>
       </div>
       <div className="column is-7 p-6">
         <h1 className="is-size-2">Only best places for your rest</h1>
@@ -25,6 +28,12 @@ const HomePage = () => (
           We will find best places for you and the conditions
           for a safe transaction and comfortable stay.
         </p>
+        <button type="button" className="start-free-btn button is-rounded is-uppercase">Start free</button>
+      </div>
+    </div>
+    <div className="columns is-centered is-vcentered home-page-before-footer">
+      <div className="column is-2 is-centered has-text-centered">
+        <h2 className="has-text-white my-3">Be everywhere at home</h2>
         <button type="button" className="start-free-btn button is-rounded is-uppercase">Start free</button>
       </div>
     </div>
