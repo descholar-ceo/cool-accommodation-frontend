f2579c7b42a41f1826bc143bf35e03c1c1b372f9
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-05 10:52:32 +0200

    Add axios and connect App component to the redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { getAllFavouritesAction } from '../redux/actions';
 import Routes from './Routes';
 
-const App = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
-);
+const App = ({ getAllFavouritesAction }) => {
+  useEffect(() => getAllFavouritesAction(), []);
+  return (
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  );
+};
 
-export default App;
+App.propTypes = {
+  getAllFavouritesAction: PropTypes.func.isRequired,
+};
+
+export default connect(null, { getAllFavouritesAction })(App);
