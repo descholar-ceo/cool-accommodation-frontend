14fad25d8ea6060487336912f3528c8ee293cf1c
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-05 10:12:40 +0200

    Add APIs

---
+++ src/assets/samples/apis.js
@@ -0,0 +1,3 @@
+export const INDEX_API = 'https://cool-accommodation-backend.herokuapp.com/';
+export const ALL_ACCOMMODATIONS = 'https://cool-accommodation-backend.herokuapp.com/accommodations';
+export const MY_FAVOURITES = myId => `https://cool-accommodation-backend.herokuapp.com/users/${myId}/favourites`;
