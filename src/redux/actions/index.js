1f45c6f69e0c19b2d3a7af34d8a55ed0f30c5539
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-05 10:30:29 +0200

    Make variable more concise

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { BASE_API, MY_FAVOURITES_API } from '../../assets/samples/apis';
-import { GET_ALL_FAVOURITES, GET_MY_FAVOURITES } from './actionsTypes';
+import { GET_ALL_FAVOURITES_ACTION, GET_MY_FAVOURITES_ACTION } from './actionsTypes';
 
 export const getAllFavouritesAction = () => async dispatch => {
   const favouriteAccomm = await axios.get(BASE_API);
-  dispatch({ cities: favouriteAccomm.data, type: GET_ALL_FAVOURITES });
+  dispatch({ allFavourites: favouriteAccomm.data, type: GET_ALL_FAVOURITES_ACTION });
 };
 export const getMyFavouritesAction = () => async dispatch => {
   const favouriteAccomm = await axios.get(MY_FAVOURITES_API);
-  dispatch({ cities: favouriteAccomm.data, type: GET_MY_FAVOURITES });
+  dispatch({ myFavourites: favouriteAccomm.data, type: GET_MY_FAVOURITES_ACTION });
 };
