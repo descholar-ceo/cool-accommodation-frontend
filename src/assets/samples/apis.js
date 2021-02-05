f928b7436d78a51acff7766fa83cde49c8eb9494
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-05 11:27:09 +0200

    Setup get accommodation profile action

--- src/assets/samples/apis.js
+++ src/assets/samples/apis.js
@@ -1,4 +1,4 @@
 export const BASE_API = 'https://cool-accommodation-backend.herokuapp.com/';
 export const ALL_ACCOMMODATIONS_API = 'https://cool-accommodation-backend.herokuapp.com/accommodations';
 export const MY_FAVOURITES_API = myId => `https://cool-accommodation-backend.herokuapp.com/users/${myId}/favourites`;
-export const ONE_ACCOMMODATION_API = accommId => `https://cool-accommodation-backend.herokuapp.com/accommodations/${accommId}`;
+export const ACCOMMODATION_PROFILE_API = accommId => `https://cool-accommodation-backend.herokuapp.com/accommodations/${accommId}`;
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
-import { BASE_API, MY_FAVOURITES_API } from '../../assets/samples/apis';
-import { GET_ALL_FAVOURITES_ACTION, GET_MY_FAVOURITES_ACTION } from './actionsTypes';
+import {
+  BASE_API, MY_FAVOURITES_API,
+  ACCOMMODATION_PROFILE_API,
+} from '../../assets/samples/apis';
+import {
+  GET_ALL_FAVOURITES_ACTION,
+  GET_MY_FAVOURITES_ACTION,
+  GET_ACCOMMODATION_PROFILE,
+} from './actionsTypes';
 
 export const getAllFavouritesAction = () => async dispatch => {
   const favouriteAccomm = await axios.get(BASE_API);
   dispatch({ allFavourites: favouriteAccomm.data, type: GET_ALL_FAVOURITES_ACTION });
 };
+
 export const getMyFavouritesAction = () => async dispatch => {
   const favouriteAccomm = await axios.get(MY_FAVOURITES_API);
   dispatch({ myFavourites: favouriteAccomm.data, type: GET_MY_FAVOURITES_ACTION });
+};
+
+export const getAccommodationProfile = accommId => async dispatch => {
+  const accommProfile = await axios.get(ACCOMMODATION_PROFILE_API(accommId));
+  dispatch({ accommodationProfile: accommProfile.data, type: GET_ACCOMMODATION_PROFILE });
 };
