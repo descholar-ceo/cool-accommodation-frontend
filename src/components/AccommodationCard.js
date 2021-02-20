9344b8f1c77c07b949b5ab7b4e30decce67d38a2
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-20 13:42:13 +0200

    Add Loading component

--- src/components/AccommodationCard.js
+++ src/components/AccommodationCard.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 
-const AccommodationCard = ({ accommodationObject }) => {};
+const AccommodationCard = ({ accommodationObject }) => {
+  const { name, price, description, rooms, city } = accommodationObject;
+  
+};
 
 AccommodationCard.propTypes = {
   accommodationObject: PropTypes.objectOf(PropTypes.object).isRequired,
