67252bdfe9e31d18456a4a6974c2c948d3f96a9e
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-20 15:05:33 +0200

    Add accommodation card

--- src/components/AccommodationCard.js
+++ src/components/AccommodationCard.js
@@ -1,8 +1,25 @@
 import PropTypes from 'prop-types';
+import sampleAccomm from '../assets/images/accommodation-7.jpg';
 
 const AccommodationCard = ({ accommodationObject }) => {
-  const { name, price, description, rooms, city } = accommodationObject;
-  
+  const { name, price, rooms } = accommodationObject;
+  return (
+    <div>
+      <div><img src={sampleAccomm} alt="Sample accomm" /></div>
+      <div>
+        <div>{name}</div>
+        <div>
+          $
+          {price}
+        </div>
+        <div>
+          {rooms}
+          {' '}
+          rooms
+        </div>
+      </div>
+    </div>
+  );
 };
 
 AccommodationCard.propTypes = {
