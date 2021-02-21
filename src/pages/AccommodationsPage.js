8d871371b286d1d29ce32b56f55f60acebd6739e
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-21 12:31:16 +0200

    Add auth

--- src/pages/AccommodationsPage.js
+++ src/pages/AccommodationsPage.js
@@ -1,13 +1,21 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import jwt from 'jsonwebtoken';
 // import { useEffect } from 'react';
 
 const AccommodationPage = ({ token }) => {
+  let decodedToken = '';
+  try {
+    decodedToken = jwt.decode(token);
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(decodedToken);
   const renderedComponent = token
     ? (
       <h1>
         Hello this is my id
-        {token}
+        {/* decodedToken */}
       </h1>
     )
     : <h1>Login first</h1>;
