5d3199a01233b3277889802b10375d264df3fe80
descholar-ceo <emmanuellamugi@gmail.com>
2021-02-22 13:38:54 +0200

    Replace the base api to heroku one

--- src/assets/samples/apis.js
+++ src/assets/samples/apis.js
@@ -1,4 +1,4 @@
-export const BASE_API = 'http://localhost:3001/';
+export const BASE_API = 'https://cool-accommodation-backend.herokuapp.com/';
 export const ALL_ACCOMMODATIONS_API = `${BASE_API}accommodations`;
 export const MY_FAVOURITES_API = myId => `${BASE_API}users/${myId}/favourites`;
 export const ACCOMMODATION_PROFILE_API = accommId => `${BASE_API}accommodations/${accommId}`;
