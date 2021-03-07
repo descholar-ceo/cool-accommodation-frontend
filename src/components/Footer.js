44ab5243246844428030ba272a15bbf6ca337cb1
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-07 18:52:30 +0200

    Add footer file

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+const Footer = () => (
+  <footer className="is-centered is-vcentered has-text-centered">
+    <p className="my-2 is-size-7">Rwanda, Western, Rubavu, Gisenyi</p>
+    <p className="my-2 is-size-7">+250701234567</p>
+    <div className="grid-3-col my-2">
+      <div className="mx-2"><a href="https://www.facebook.com/"><FaFacebook /></a></div>
+      <div className="mx-2"><a href="https://twitter.com/"><FaTwitter /></a></div>
+      <div className="mx-2"><a href="https://www.instagram.com/"><FaInstagram /></a></div>
+    </div>
+    <p className="my-2 is-size-7">CoolAccommodation 2021. All right reserved</p>
+  </footer>
+);
+
+export default Footer;
