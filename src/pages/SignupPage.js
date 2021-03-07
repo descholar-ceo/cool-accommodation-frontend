9d7840ab86187f8a81a1e9aaec818ee69baf905a
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-07 23:25:38 +0200

    Fix the signup form

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,4 +1,3 @@
-import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
@@ -8,7 +7,7 @@ const SignupPage = () => (
   <>
     <Navbar bg="has-background-primary-dark" />
     <div className="columns is-centered is-vcentered height-70 login-container">
-      <div className="column is-4 login-component">
+      <div className="login-component p-4">
         <TextInput inputType="email" placeholder="email" />
         <TextInput inputType="password" placeholder="password" />
         <TextInput inputType="password" placeholder="confirm password" />
