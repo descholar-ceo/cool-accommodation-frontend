96f11a7bf8b6322d259fff1f598e3580e6160044
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-08 09:38:22 +0200

    Add sample accommodations

--- src/pages/AccommodationsPage.js
+++ src/pages/AccommodationsPage.js
@@ -1,7 +1,12 @@
-const AccommodationPage = () => (
-  <div>
-    <h2>Hey! Welcome to the new world!</h2>
-  </div>
-);
+import allAccommodations from '../assets/samples/sample-accommodations';
+
+const AccommodationPage = () => {
+  console.log({ accomm: allAccommodations() });
+  return (
+    <div>
+      <h2>Hey! Welcome to the new world!</h2>
+    </div>
+  );
+};
 
 export default AccommodationPage;
