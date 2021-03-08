98178bd393d231f4e211a14cb7204937b0120e97
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-08 09:22:37 +0200

    Fix the failing props in accommodations page

--- src/pages/AccommodationsPage.js
+++ src/pages/AccommodationsPage.js
@@ -1,40 +1,7 @@
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import jwt from 'jsonwebtoken';
-import { useEffect } from 'react';
-import { getAllAccommodations } from '../redux/actions';
+const AccommodationPage = () => (
+  <div>
+    <h2>Hey! Welcome to the new world!</h2>
+  </div>
+);
 
-const AccommodationPage = ({ token, getAllAccommodations }) => {
-  let decodedToken = '';
-  try {
-    decodedToken = jwt.decode(token);
-    useEffect(() => getAllAccommodations(token), []);
-  } catch (err) {
-    // console.log(err);
-  }
-  // console.log(decodedToken);
-  const renderedComponent = token
-    ? (
-      <h1>
-        Hello this is my id
-        {decodedToken.id}
-      </h1>
-    )
-    : <h1>Login first</h1>;
-  return (
-    <div>
-      { renderedComponent }
-    </div>
-  );
-};
-
-AccommodationPage.propTypes = {
-  token: PropTypes.string.isRequired,
-  getAllAccommodations: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = state => ({
-  token: state.loginReducer.token,
-});
-
-export default connect(mapStateToProps, { getAllAccommodations })(AccommodationPage);
+export default AccommodationPage;
