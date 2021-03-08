807457930820719bce04d53d4836fd673015e67c
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-08 20:11:22 +0200

    Add navbar

--- src/pages/AccommodationsPage.js
+++ src/pages/AccommodationsPage.js
@@ -1,6 +1,7 @@
 import allAccommodations from '../assets/samples/sample-accommodations';
 import AccommodationCard from '../components/AccommodationCard';
 import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const AccommodationPage = () => {
   const accommodationsList = allAccommodations.map(accomm => (
@@ -8,6 +9,7 @@ const AccommodationPage = () => {
 
   return (
     <div className="">
+      <Navbar />
       <div className="m-2">
         { accommodationsList }
       </div>
