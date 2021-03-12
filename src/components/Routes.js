44457de91fd40b0bb8ed40a97a8c9024060b4d31
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-12 11:41:41 +0200

    Add routes to the details page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -4,6 +4,7 @@ import Fourofourpage from '../pages/Fourofourpage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
 import AccommodationPage from '../pages/AccommodationsPage';
+import AccommodationDetails from '../pages/AccommodationDetails';
 
 const Routes = () => (
   <Switch>
@@ -11,6 +12,7 @@ const Routes = () => (
     <Route exact path="/signin" component={LoginPage} />
     <Route exact path="/signup" component={SignupPage} />
     <Route exact path="/accommodations" component={AccommodationPage} />
+    <Route exact path="/details/:accommodation" component={AccommodationDetails} />
     <Route component={Fourofourpage} />
   </Switch>
 );
