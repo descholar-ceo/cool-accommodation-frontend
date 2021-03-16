6788448ae8decdfab33e6c3be19e936eb41bae14
descholar-ceo <emmanuellamugi@gmail.com>
2021-03-16 18:43:13 +0200

    Add favourite accommodation

--- src/assets/samples/apis.js
+++ src/assets/samples/apis.js
@@ -2,3 +2,4 @@ export const BASE_API = 'https://cool-accommodation-backend.herokuapp.com/';
 export const ALL_ACCOMMODATIONS_API = `${BASE_API}accommodations`;
 export const MY_FAVOURITES_API = myId => `${BASE_API}users/${myId}/favourites`;
 export const LOGIN_API = `${BASE_API}login`;
+export const ADD_ACCOMMODATION_TO_FAVOURITES = `${BASE_API}favourites`;
