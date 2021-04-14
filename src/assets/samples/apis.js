ac0bff76407bd966f14ad64a530b68af6b0ad44a
descholar-ceo <emmanuellamugi@gmail.com>
2021-04-14 10:20:02 +0200

    Fix the addfav accommodations action

--- src/assets/samples/apis.js
+++ src/assets/samples/apis.js
@@ -2,5 +2,5 @@ export const BASE_API = 'https://cool-accommodation-backend.herokuapp.com/';
 export const ALL_ACCOMMODATIONS_API = `${BASE_API}accommodations`;
 export const MY_FAVOURITES_API = myId => `${BASE_API}users/${myId}/favourites`;
 export const LOGIN_API = `${BASE_API}login`;
-export const ADD_ACCOMMODATION_TO_FAVOURITES_API = `${BASE_API}favourites`;
+export const ADD_ACCOMMODATION_TO_FAVOURITES_API = myId => `${BASE_API}users/${myId}/favourites`;
 export const REMOVE_ACCOMMODATION_FROM_FAVOURITES_API = favId => `${BASE_API}favourites/${favId}`;
