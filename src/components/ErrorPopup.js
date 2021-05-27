045b3360aee230241c3147bd50dfaa0d8e7f980e
descholar-ceo <emmanuellamugi@gmail.com>
2021-05-27 22:53:05 +0200

    Remove console statement

--- src/components/ErrorPopup.js
+++ src/components/ErrorPopup.js
@@ -1,17 +1,14 @@
 import PropTypes from 'prop-types';
 
-const ErrorPopup = ({ error }) => {
-  console.log(error);
-  return (
-    <div className="modal">
-      <div className="modal-background" />
-      <div className="modal-content">
-        {error}
-      </div>
-      <button type="button" className="modal-close is-large" aria-label="close" />
+const ErrorPopup = ({ error }) => (
+  <div className="modal">
+    <div className="modal-background" />
+    <div className="modal-content">
+      {error}
     </div>
-  );
-};
+    <button type="button" className="modal-close is-large" aria-label="close" />
+  </div>
+);
 
 ErrorPopup.propTypes = {
   error: PropTypes.objectOf().isRequired,
