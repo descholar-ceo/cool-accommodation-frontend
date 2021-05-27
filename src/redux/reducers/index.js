81d95a3216a1f007fab8217f9fc4904ac95d1a39
descholar-ceo <emmanuellamugi@gmail.com>
2021-05-27 19:07:35 +0200

    Export signup reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,5 +2,8 @@ import { combineReducers } from 'redux';
 import favouritesReducer from './favourites';
 import loginReducer from './login';
 import accommodationsReducer from './accommodation';
+import signupReducer from './signup';
 
-export default combineReducers({ favouritesReducer, loginReducer, accommodationsReducer });
+export default combineReducers({
+  favouritesReducer, loginReducer, accommodationsReducer, signupReducer,
+});
