6ec5f88d93d1a0e8a59e2e9bbf1e6050870b12fe
descholar-ceo <emmanuellamugi@gmail.com>
2021-05-27 19:05:00 +0200

    Add signup reducer

---
+++ src/redux/reducers/signup.js
@@ -0,0 +1,19 @@
+import { GET_ERRORS_ACTION, SIGNUP_ACTION } from '../actions/actionsTypes';
+
+const initialState = {
+  token: '',
+  error: '',
+};
+
+const loginReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGNUP_ACTION:
+      return { ...state, token: action.token };
+    case GET_ERRORS_ACTION:
+      return { ...state, error: action.error };
+    default:
+      return state;
+  }
+};
+
+export default loginReducer;
